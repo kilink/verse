f0e79c108e65c223c7f70e3349d05e32ecff9c53
Patrick Strawderman <pstrawderman@netflix.com>
2021-11-23 11:55:30 -0800

    Update to Kotlin 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.6.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
     id("java")
     id("java-library")
